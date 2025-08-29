35d5399eaa401bf308bf3e94ecff73aaf8e906fc
raileats <raileats@gmail.com>
2025-08-30 03:08:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { appDir: true },
-  typescript: { ignoreBuildErrors: false }
-};
+const nextConfig = {};
 export default nextConfig;
